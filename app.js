267d4ac80419a37610b53d25114bf4e858e731e9
Michael Sundius <msundius@sundius.com>
2025-04-11 16:09:51 -0400

    add some debug

--- app.js
+++ app.js
@@ -32,74 +32,64 @@ const io = require('socket.io')(app);
 app.listen(8080);
 console.log('Pi Car web server listening on port 8080 visit http://<ip>:8080/socket.html');
 
-// Connect to pigpiod
 const pi = pigpio();
-console.log('pigpio and Pi Car web server ');
+const throttle = pi.gpio(17);
+const steering = pi.gpio(18);
+throttle.modeSet('output');
+steering.modeSet('output');
 
-//pi.connect(() => {
-  const throttle = pi.gpio(17);
-  const steering = pi.gpio(18);
-
-  console.log(`11 pi connect`);
-  throttle.modeSet('output');
-  steering.modeSet('output');
-
-  function setServo(gpio, pwmValue) {
-    const pulse = Math.round(1000 + ((pwmValue - pwm_min) / (pwm_max - pwm_min)) * 1000);
-    gpio.servoWrite(pulse);
-  }
-
-  function emergencyStop() {
-    setServo(throttle, pwm_neutral);
-    setServo(steering, pwm_neutral);
-    console.log('###EMERGENCY STOP - signal lost or shutting down');
-  }
-
-  console.log(`11 pi functions`);
-
-  let smoothed_throttle = pwm_neutral;
-  let logcount = 0;
-  old_gamma = pwm_neutral;
-  old_beta = pwm_neutral;
-  let lastAction = null;
-
-  console.log(`11 pi stuff`);
-
-  io.on('connection', (socket) => {
-    console.log(`11 connect`);
-    socket.on('fromclient', (data) => {
-      console.log(`22 Beta: ${data.beta} Gamma: ${data.gamma} smoothed: ${smoothed_throttle}`);
-      logcount++;
-      old_beta = data.beta;
-      old_gamma = data.gamma;
-
-      if (data.gamma > pwm_neutral && data.gamma > smoothed_throttle) {
-        if (smoothed_throttle < pwm_neutral) smoothed_throttle = pwm_neutral;
-        smoothed_throttle += 0.001;
-      } else if (data.gamma < pwm_neutral && data.gamma < smoothed_throttle) {
-        if (smoothed_throttle > pwm_neutral) smoothed_throttle = pwm_neutral;
-        smoothed_throttle -= 0.0003;
-      } else {
-        smoothed_throttle = data.gamma;
-      }
-
-      if (logcount == 10) {
-        logcount = 0;
-        console.log(`Beta: ${data.beta} Gamma: ${data.gamma} smoothed: ${smoothed_throttle}`);
-      }
-
-      setServo(throttle, smoothed_throttle);
-      setServo(steering, data.beta);
-
-      clearInterval(lastAction);
-      lastAction = setInterval(emergencyStop, 2000);
-    });
+function setServo(gpio, pwmValue) {
+  const pulse = Math.round(1000 + ((pwmValue - pwm_min) / (pwm_max - pwm_min)) * 1000);
+  console.log(`gpio:pulse is ${pulse}`);
+  gpio.setServoPulsewidth(pulse, err => {
+    if (err) console.error(`Failed to set servo pulse: ${err.message}`);
   });
+}
 
-  process.on('SIGINT', function () {
+function emergencyStop() {
+  setServo(throttle, pwm_neutral);
+  setServo(steering, pwm_neutral);
+  console.log('###EMERGENCY STOP - signal lost or shutting down');
+}
+
+let smoothed_throttle = pwm_neutral;
+let logcount = 0;
+let lastAction = null;
+
+io.on('connection', (socket) => {
+  console.log(`hello connect`);
+  socket.on('fromclient', (data) => {
+    console.log(`init socket `);
+
+    logcount++;
+    old_beta = data.beta;
+    old_gamma = data.gamma;
+
+    if (data.gamma > pwm_neutral && data.gamma > smoothed_throttle) {
+      if (smoothed_throttle < pwm_neutral) smoothed_throttle = pwm_neutral;
+      smoothed_throttle += 0.001;
+    } else if (data.gamma < pwm_neutral && data.gamma < smoothed_throttle) {
+      if (smoothed_throttle > pwm_neutral) smoothed_throttle = pwm_neutral;
+      smoothed_throttle -= 0.0003;
+    } else {
+      smoothed_throttle = data.gamma;
+    }
+
+    if (logcount === 10) {
+      logcount = 0;
+      console.log(`Beta: ${data.beta} Gamma: ${data.gamma} smoothed: ${smoothed_throttle}`);
+    }
+
+    setServo(throttle, smoothed_throttle);
+    setServo(steering, data.beta);
+
+    clearInterval(lastAction);
+    lastAction = setInterval(emergencyStop, 2000);
+  });
+});
+
+process.on('SIGINT', function () {
     emergencyStop();
     console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
     process.exit();
   });
-//});
-  
