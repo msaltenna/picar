d7571687cce5332a4173139ae63abe1b297a228c
Michael Sundius <msundius@sundius.com>
2025-04-11 13:27:16 -0400

    fixes

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const { pwm_min, pwm_max, pwm_neutral } = config;
 // Static file server
 const file = new static.Server();
 
+// Declare beta/gamma globally so /status can access them
+let old_gamma = pwm_neutral;
+let old_beta = pwm_neutral;
+
 // Create HTTP + WebSocket server
 const app = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
@@ -29,61 +33,73 @@ app.listen(8080);
 console.log('Pi Car web server listening on port 8080 visit http://<ip>:8080/socket.html');
 
 // Connect to pigpiod
-const pi = pigpio({ host: 'localhost' });
-const throttle = pi.gpio(17);
-const steering = pi.gpio(18);
+const pi = pigpio();
+console.log('pigpio and Pi Car web server ');
 
-throttle.modeSet('output');
-steering.modeSet('output');
+//pi.connect(() => {
+  const throttle = pi.gpio(17);
+  const steering = pi.gpio(18);
 
-function setServo(pin, pwmValue) {
-  const pulse = Math.round(1000 + ((pwmValue - pwm_min) / (pwm_max - pwm_min)) * 1000);
-  pin.servoWrite(pulse);
-}
+  console.log(`11 pi connect`);
+  throttle.modeSet('output');
+  steering.modeSet('output');
 
-let smoothed_throttle = pwm_neutral;
-let logcount = 0;
-let old_gamma = pwm_neutral;
-let old_beta = pwm_neutral;
-let lastAction = null;
+  function setServo(gpio, pwmValue) {
+    const pulse = Math.round(1000 + ((pwmValue - pwm_min) / (pwm_max - pwm_min)) * 1000);
+    gpio.servoWrite(pulse);
+  }
 
-function emergencyStop() {
-  setServo(throttle, pwm_neutral);
-  setServo(steering, pwm_neutral);
-  console.log('###EMERGENCY STOP - signal lost or shutting down');
-}
+  function emergencyStop() {
+    setServo(throttle, pwm_neutral);
+    setServo(steering, pwm_neutral);
+    console.log('###EMERGENCY STOP - signal lost or shutting down');
+  }
 
-io.on('connection', (socket) => {
-  socket.on('fromclient', (data) => {
-    logcount++;
-    old_beta = data.beta;
-    old_gamma = data.gamma;
+  console.log(`11 pi functions`);
 
-    if (data.gamma > pwm_neutral && data.gamma > smoothed_throttle) {
-      if (smoothed_throttle < pwm_neutral) smoothed_throttle = pwm_neutral;
-      smoothed_throttle += 0.001;
-    } else if (data.gamma < pwm_neutral && data.gamma < smoothed_throttle) {
-      if (smoothed_throttle > pwm_neutral) smoothed_throttle = pwm_neutral;
-      smoothed_throttle -= 0.0003;
-    } else {
-      smoothed_throttle = data.gamma;
-    }
+  let smoothed_throttle = pwm_neutral;
+  let logcount = 0;
+  old_gamma = pwm_neutral;
+  old_beta = pwm_neutral;
+  let lastAction = null;
 
-    if (logcount === 10) {
-      logcount = 0;
-      console.log(`Beta: ${data.beta} Gamma: ${data.gamma} smoothed: ${smoothed_throttle}`);
-    }
+  console.log(`11 pi stuff`);
 
-    setServo(throttle, smoothed_throttle);
-    setServo(steering, data.beta);
+  io.on('connection', (socket) => {
+    console.log(`11 Beta: ${data.beta} Gamma: ${data.gamma} smoothed: ${smoothed_throttle}`);
+    socket.on('fromclient', (data) => {
+      console.log(`22 Beta: ${data.beta} Gamma: ${data.gamma} smoothed: ${smoothed_throttle}`);
+      logcount++;
+      old_beta = data.beta;
+      old_gamma = data.gamma;
 
-    clearInterval(lastAction);
-    lastAction = setInterval(emergencyStop, 2000);
+      if (data.gamma > pwm_neutral && data.gamma > smoothed_throttle) {
+        if (smoothed_throttle < pwm_neutral) smoothed_throttle = pwm_neutral;
+        smoothed_throttle += 0.001;
+      } else if (data.gamma < pwm_neutral && data.gamma < smoothed_throttle) {
+        if (smoothed_throttle > pwm_neutral) smoothed_throttle = pwm_neutral;
+        smoothed_throttle -= 0.0003;
+      } else {
+        smoothed_throttle = data.gamma;
+      }
+
+      if (logcount == 10) {
+        logcount = 0;
+        console.log(`Beta: ${data.beta} Gamma: ${data.gamma} smoothed: ${smoothed_throttle}`);
+      }
+
+      setServo(throttle, smoothed_throttle);
+      setServo(steering, data.beta);
+
+      clearInterval(lastAction);
+      lastAction = setInterval(emergencyStop, 2000);
+    });
   });
-});
 
-process.on('SIGINT', function () {
-  emergencyStop();
-  console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
-  process.exit();
-});
+  process.on('SIGINT', function () {
+    emergencyStop();
+    console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
+    process.exit();
+  });
+//});
+  
